9a2975ec8d9c1412336bd7624132ff792eb9db55
William Chen <williamchen16@gmail.com>
2013-03-15 15:04:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuddyBuildTracker::Application.config.secret_token = 'd5b7eeec9de63f15338179e56d680106936ed41b911776735f04dd109933cfe03fbbd1dee42cbae39adcea054a73aabff70b10c99bac00354347025c5d5dc228'

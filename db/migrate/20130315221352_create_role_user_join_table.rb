517ca913893c45eb12f0983067faa87bc56ad8b9
William Chen <williamchen16@gmail.com>
2013-03-15 15:15:29 -0700

    Role and User models and join table; HABTM relationship
    Did I mention how much I hate the pluralize convention thing?
    Keeps messing me up. Grrrr

---
+++ db/migrate/20130315221352_create_role_user_join_table.rb
@@ -0,0 +1,11 @@
+class CreateRoleUserJoinTable < ActiveRecord::Migration
+  def change
+    create_table :roles_users, :id => false do |t|
+      t.references :role
+      t.references :user
+    end
+
+    add_index :roles_users, [:role_id, :user_id]
+    add_index :roles_users, [:user_id, :role_id]
+  end
+end
